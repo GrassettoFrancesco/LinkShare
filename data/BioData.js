402e5d2a0914f6b1e8548ffc3b37bc1b008e273b
Francesco Grassetto <100849617+GrassettoFrancesco@users.noreply.github.com>
2023-08-30 10:31:41 +0200

    Update BioData.js

--- data/BioData.js
+++ data/BioData.js
@@ -1,20 +1,20 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
-        url: 'https://nxtlnk.vercel.app',
+        name: 'Francesco Grassetto',
+        username: 'cesco.vercel.app',
+        url: 'https://cesco.vercel.app',
         titleImg: true,
         avatar: '/avatar.png',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        description: 'Italian Content Creator',
         descShow: true,
-        subdesc: 'Use this template and deploy on vercel for free',
+        subdesc: 'Italy',
         subdescShow: true,
         newProductUrl: 'https://3dicons.co',
         newProduct: true,
-        author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
+        author: 'cesco',
+        authorURL: 'https://youtube.com/@grassettofrancesco',
         footerText: 'made by'
     },
 ];
